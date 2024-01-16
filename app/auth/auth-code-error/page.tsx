860c26aee91977044fba5eae21ccd9aeb81816c9
Pranav10291 <prungta03@gmail.com>
2024-01-17 02:15:59 +0530

    Deleted test page

--- app/auth/auth-code-error/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Page() {
-    return (
-        <div>
-            <h1 className="text-2xl font-bold">Error in auth</h1>
-        </div>
-    );
-}

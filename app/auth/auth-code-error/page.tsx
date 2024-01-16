a46c5b7400052a2109f2b2f1fe8d77dd7cef5aa8
Pranav10291 <prungta03@gmail.com>
2024-01-17 01:52:28 +0530

    Sign in with Google

---
+++ app/auth/auth-code-error/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Page() {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">Error in auth</h1>
+        </div>
+    );
+}

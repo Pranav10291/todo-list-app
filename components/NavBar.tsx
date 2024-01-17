ef628d51c63ebf81231f998b408435770c2ce5c1
Pranav10291 <prungta03@gmail.com>
2024-01-17 20:12:53 +0530

    Navbar changes

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
-import { HandMetal } from "lucide-react";
 import SignOut from "./SignOut";
+import readUserSession from "@/lib/actions";
+import { redirect } from "next/navigation";
 
-const NavBar = () => {
+const NavBar = async () => {
+    const { data } = await readUserSession();
+    if (!data.session) {
     return (
         <div className=" bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0 ">
             <div className="flex flex-wrap items-center justify-between mx-10">
@@ -27,13 +30,38 @@ const NavBar = () => {
                             Sign in
                         </Link>
                     </div>
-                    <div>
-                        <SignOut />
-                    </div>
                 </div>
             </div>
         </div>
     );
+    }
+    else{
+        return (
+            <div className=" bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0 ">
+                <div className="flex flex-wrap items-center justify-between mx-10">
+                    <div className="flex items-center flex-shrink-0">
+                        <Link href="/">
+                            <img
+                                src="taskify_title.png"
+                                alt="HomePage"
+                                className="h-10"
+                            />
+                        </Link>
+                    </div>
+                    <div className=" flex flex-wrap flex-shrink-0 items-center space-x-4">
+                        <div className=" hover:bg-gray-800 hover:text-white hover:shadow hover: rounded-lg py-1.5 px-2">
+                            <Link href="/todo">
+                                Tasks
+                            </Link>
+                        </div>
+                        <div>
+                            <SignOut />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default NavBar;

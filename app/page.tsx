473c60b589815bbcc493491483febb8c15d03fbf
Pranav10291 <prungta03@gmail.com>
2024-01-18 19:16:07 +0530

    Retrying Sign in with google

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import readUserSession from "@/lib/actions";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+    const {data}= await readUserSession()
+	if(data.session){
+		return redirect("/todo")
+	}
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-5">
             <div className="pt-36">

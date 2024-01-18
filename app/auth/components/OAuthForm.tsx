608a0d433a1afbf07cdee1f891eb184f49725f09
Pranav10291 <prungta03@gmail.com>
2024-01-18 19:58:17 +0530

    Google

--- app/auth/components/OAuthForm.tsx
+++ app/auth/components/OAuthForm.tsx
@@ -1,15 +1,22 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import React from "react";
-import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from "@supabase/supabase-js";
 
 export default function OAuthForm() {
-	const supabase = createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+	const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 
 	const signInWithGoogle = async () => {
 		let { data, error } = await supabase.auth.signInWithOAuth({
 			provider: 'google',
-		})
+		});
+		if (error) {
+			console.log(error)
+			return
+		}
+		else {
+			console.log(data)
+		}
 	};
 
 	return <Button onClick={signInWithGoogle} className="w-full">Login With Google</Button>;
